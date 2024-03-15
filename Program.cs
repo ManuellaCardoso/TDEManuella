17a461c88e6313c861154f4d0b292001a7cd95cd
Manuella S. Cardoso <163538424+ManuellaCardoso@users.noreply.github.com>
2024-03-15 15:32:51 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//Criando as variaveis e exibindo seus valores
+
+int idade = 35;
+string nome = "Maria";
+double altura = 3.45;
+const int ano = 12;
+double? nota = 7.80;
+
+Console.WriteLine(idade);  
+Console.WriteLine(nome);   
+Console.WriteLine(altura); 
+Console.WriteLine(ano);    
+Console.WriteLine(nota);
+
+
+//Perguntas e respostas:
+//P: 6. Quais as diferenças entre os tipos por valor e os tipos por referência ?
+//R: Os tipos por valor armazenam diretamente seus próprios dados, enquanto tipos por referência armazenam uma referência aonde os dados estão armazenados na memória.
+
+//P: 7. O que é um nullable type e qual a sua utilidade ?
+//R: Um Nullable type permite que os tipos de valores aceitem um valor nulo, representando a ausencia de valor.
+
+//P: 8. O que é Camel Case ? Dê um exemplo de sua aplicação.
+//R: Camel Case é uma conveção de nomenclatura onde a primeira letra da primeira palavra é minùscula, e a primeira letra de cada palavra a seguir é aiúscula, sem espaços entre elas, por exemplo: nomeDeVariavel.
+
+//P: 9. O que é Pascal Case ? Dê um exemplo de sua aplicação.
+//R: Pascal Case é uma convenção de nomenclatura onde a primeira letra da primeira palavra é maiúscula, sem espaços entre elas, por exemplo: NomeDeVariavel.
+
+//P: 10. Declare duas variáveis x e y como sendo do tipo int e atribua a ela os valores 77 e 66 e a seguir imprima o valor da soma de x com y. (Use o operador aritmético + para realizar a operação de soma).
+//R: 
+
+int x = 77;
+int y = 66;
+
+Console.WriteLine("A soma de x + y é:" +(x+y));
+
+//P: 11. Quais os valores padrões dos tipos de dados bool, char, int, double, float, decimal e string.
+//R: bool: false
+// char: '\0'
+// int: 0
+// double: 0.0
+// float: 0.0f
+// decimal: 0.0m
+// string: null 
+
